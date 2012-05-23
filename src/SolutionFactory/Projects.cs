cdd42e3c6be4bdf15f7f80b6887e5bee83709188
Eric Hexter <eric.hexter@gmail.com>
2012-05-22 21:46:51 -0500

    initial commit
    brought over from a fork of the codeplex project.... RIP old project

---
+++ src/SolutionFactory/Projects.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Linq;
+using EnvDTE;
+using EnvDTE80;
+using VSLangProj;
+
+namespace SolutionFactory
+{
+    public static class Projects
+    {
+        public static void AddProjectFromTemplate(object dte, string templateName, string projectName)
+        {
+            var dte1 = (DTE)dte;
+            string path = Path.GetDirectoryName(dte1.Solution.FileName);
+            var solution2 = ((Solution2) dte1.Solution);
+            string template = solution2.GetProjectTemplate(templateName, "CSharp");
+            solution2.AddFromTemplate(template, path + "\\" + projectName, projectName);
+        }
+
+        public static void AddProject(object dte, string projectName)
+        {
+            AddProjectFromTemplate(dte, "ClassLibrary.zip", projectName);
+        }
+
+        public static void AddReference(object dteObject, string target, string reference)
+        {
+            DTE dte = (DTE)dteObject;
+            var targetProject = (VSProject) dte.GetProject(target).Object;
+            var referenceProject = dte.GetProject(reference);
+            targetProject.References.AddProject(referenceProject);
+        }
+
+        public static void AddLibraryReference(object dteObject, string target, string library)
+        {
+            DTE dte = (DTE)dteObject;
+            var targetProject = (VSProject)dte.GetProject(target).Object;
+            targetProject.References.Add(library);
+        }
+
+        public static void RemoveLibraryReference(object dteObject, string target, string library)
+        {
+            DTE dte = (DTE)dteObject;
+            var targetProject = (VSProject)dte.GetProject(target).Object;
+            var refToRemove = targetProject.References.Cast<Reference>().Where(assembly => assembly.Name.EndsWith(library, System.StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            if (refToRemove != null)
+            {
+                refToRemove.Remove();
+            }
+        }
+
+        private static Project GetProject(this DTE dte, string target)
+        {
+            return dte.Solution.Projects.Cast<Project>().Where(project => project.Name.Equals(target,System.StringComparison.InvariantCultureIgnoreCase)).First();
+        }
+
+        public static void SetNamespace(object dteObject,string project,string defaultNamespace)
+        {
+            var dte = (DTE)dteObject;
+            dte.GetProject(project).Properties.Item("DefaultNamespace").Value = defaultNamespace;
+        }
+    }
+}
---
+++ src/SolutionFactory/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Solution Factory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Solution Factory")]
+[assembly: AssemblyProduct("Solution Factory")]
+[assembly: AssemblyCopyright("Copyright © Eric Hexter 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("f0269ae7-2c80-4f71-a732-3078f1f958f5")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
